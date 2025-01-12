bcb79c5abf7776a8932508d2a4494ef1be9d966b
Sagid Kadirov <sagidkadirov1999@gmail.com>
2025-01-12 19:46:25 +0500

    configuration files created

---
+++ src/main/java/ru/kadirov/firstproject/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,34 @@
+package ru.kadirov.firstproject.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenHttpMethodFilter(servletContext);
+    }
+
+    private void registerHiddenHttpMethodFilter(ServletContext servletContext) {
+        servletContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+
+    }
+}
